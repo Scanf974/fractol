9e9d780a0a9ac938fc6e1f87628612f69076c02d
bsautron <sautron.brunojj@gmail.com>
2015-03-23 20:57:49 +0100

    iure

--- ft_mandelbrot.c
+++ ft_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/23 15:48:36 by bsautron          #+#    #+#             */
-/*   Updated: 2015/03/23 20:52:52 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/03/23 20:57:36 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,8 @@
 
 static void	ft_init_mandel(t_env *env)
 {
-	env->zoom = 2.2;
+	env->zoom = 1;
 	env->mandel.iteration_max = 128;
-	env->mandel.lim_x1 = -2.1;
-	env->mandel.lim_x2 = 0.6;
-	env->mandel.lim_y1 = -1.2;
-	env->mandel.lim_y2 = 1.2;
-	env->mandel.image_x = (env->mandel.lim_x2 - env->mandel.lim_x1) * env->zoom;
-	env->mandel.image_y = (env->mandel.lim_y2 - env->mandel.lim_y1) * env->zoom;
-	//env->mandel.zoom_x = WIDTH / (env->mandel.lim_x2 - env->mandel.lim_x1);
-	//env->mandel.zoom_y = 240 / (env->mandel.lim_y2 - env->mandel.lim_y1);
-	//dprintf(1, "%f %f\n", WIDTH / (env->mandel.lim_x2 - env->mandel.lim_x1), env->mandel.zoom_x);
 }
 
 static void	ft_each(int y, int x, t_env *env)
@@ -37,7 +28,6 @@ static void	ft_each(int y, int x, t_env *env)
 
 	c.re = 1.5 * (x - WIDTH / 2) / ( 0.5 * env->zoom * WIDTH) - 0.5;
 	c.im = (y - HEIGHT / 2) / (0.5 * env->zoom * HEIGHT) + 0;
-	dprintf(1, "%f\n", c.im);
 	new.re = 0;
 	new.im = 0;
 	old.re = 0;
@@ -53,11 +43,9 @@ static void	ft_each(int y, int x, t_env *env)
 			break ;
 		i++;
 	}
-	if (i != env->mandel.iteration_max)
+	if (i == env->mandel.iteration_max)
 	{
-		env->img.char_img[ x * 4 + y * env->img.size_line] = i % 256;
-		env->img.char_img[ x * 4 + y * env->img.size_line + 1] = 255;
-		env->img.char_img[ x * 4 + y * env->img.size_line + 2] = 255 * (i < env->mandel.iteration_max);
+		ft_put_pixel_to_image(env, y, x, 0xffffff);
 	}
 }
 
@@ -67,15 +55,15 @@ void	ft_mandelbrot(t_env *env)
 	int		y;
 
 	ft_init_mandel(env);
-	x = 0;
-	while (x < WIDTH)
+	y = 0;
+	while (y < HEIGHT)
 	{
-		y = 0;
-		while (y < HEIGHT)
+		x = 0;
+		while (x < WIDTH)
 		{
 			ft_each(y, x, env);
-			y++;
+			x++;
 		}
-		x++;
+		y++;
 	}
 }
